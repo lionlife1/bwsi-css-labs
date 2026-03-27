4dde163ea1e582c7dab640c098c40e4175cf2214
Aarav Mehta <aaravjmehta@gmail.com>
2026-03-26 19:29:12 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "AJMehta" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Intro goes here")
 
 if __name__ == "__main__":
     main()
